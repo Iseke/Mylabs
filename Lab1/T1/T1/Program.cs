006acb2b0c2e38bb15b976999a0a3169b2a166b8
Iseke <islamabdukarimov1999@mail.ru>
2018-01-13 18:57:13 +0600

    islamlol

---
+++ Lab1/T1/T1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            //List<int> isa = new List<int>();
+            string s = Console.ReadLine();
+            string[] val = s.Split(' ');
+            int n = val.Length;
+            for (int i = 0; i < n; i++)
+            {
+                if (pros(int.Parse(val[i])))
+                {
+                    Console.WriteLine(val[i]);
+                }
+            }//lol
+
+
+
+        }
+        static bool pros(int x)
+        {
+            if (x == 0) return false;
+            for (int i = 2; i <= Math.Sqrt(x); i++)
+            {
+                if (x % i == 0) return false;
+            }
+            return true;
+
+        }
+    }
+}
--- Lab1/T1/T1/Properties/AssemblyInfo.cs
+++ Lab1/T1/T1/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Task1")]
+[assembly: AssemblyTitle("T1")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Task1")]
+[assembly: AssemblyProduct("T1")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("2492199c-c8c9-4b7c-9694-ec11542ff741")]
+[assembly: Guid("bf60b7bf-2a79-4e66-bdf8-e48901d4f207")]
 
 // Version information for an assembly consists of the following four values:
 //
