286da960871faf0c3b455922b0614c55ee76eab1
Iseke <islamabdukarimov1999@mail.ru>
2018-01-14 00:22:15 +0600

    rectangle

---
+++ Lab1/ractangle/ractangle/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ractangle
+{
+    class pryamougolnik
+    {
+      public  string  width;
+       public string  height;
+        public void cinInfo()
+        {
+            Console.WriteLine("Please write a width and height:");
+
+            width = Console.ReadLine();
+
+            height = Console.ReadLine();
+         
+        }
+        public void findArea()
+        {
+            int a = int.Parse(width);
+            int b = int.Parse(height);
+            Console.WriteLine("Area:" + a * b);
+
+        }
+        public void findaPerimetr()
+        {
+            int a = int.Parse(width);
+            int b = int.Parse(height);
+            Console.WriteLine("Perimetr:" + 2*(a + b));
+
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            pryamougolnik s = new pryamougolnik();
+            s.cinInfo();
+            s.findaPerimetr();
+            s.findArea();
+        }
+    }
+}
