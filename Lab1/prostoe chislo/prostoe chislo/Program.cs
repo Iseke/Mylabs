38d52fa17c33fda2084326dbee26c655f0208002
Iseke <islamabdukarimov1999@mail.ru>
2018-01-14 00:07:14 +0600

    prost

---
+++ Lab1/prostoe chislo/prostoe chislo/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+            string[] val = s.Split(' ');
+            int n = val.Length;
+            for (int i = 0; i < n; i++)
+            {
+                if (pros(int.Parse(val[i])))
+                {
+                    Console.WriteLine(val[i]);
+                }
+            }
+
+
+
+        }
+        static bool pros(int x)
+        {
+            if (x <= 0) return false;
+            for (int i = 2; i <= Math.Sqrt(x); i++)
+            {
+                if (x % i == 0) return false;
+            }
+            return true;
+
+        }
+    }
+}
