5ed77984898747e201e22f39c8f42143706dd30d
iseke <islamabdukarimov1999@mail.ru>
2018-02-28 21:22:17 +0600

    Last

---
+++ Lab4/Complex/Complex/Cplex.cs
@@ -0,0 +1,420 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+
+
+namespace complex
+
+{
+
+    [Serializable]
+
+    public class Cplex
+
+    {
+
+        public int a, b;
+
+
+
+        public Cplex() { }
+
+        public Cplex(int _a, int _b)
+
+        {
+
+            a = _a;
+
+            b = _b;
+
+        }
+
+
+
+        public static Cplex operator +(Cplex x, Cplex y)
+
+        {
+
+            Cplex res = new Cplex();
+
+
+
+            res.a = y.b * x.a + x.b * y.a;
+
+            res.b = x.b * y.b;
+
+            if (res.a > res.b)
+
+            {
+
+                for (int i = res.b; i > 0; --i)
+
+                {
+
+                    if (res.a % i == 0 && res.b % i == 0)
+
+                    {
+
+                        res.a = res.a / i;
+
+                        res.b = res.b / i;
+
+                    }
+
+                }
+
+            }
+
+            else if (res.a < res.b)
+
+            {
+
+                for (int i = res.a; i > 0; --i)
+
+                {
+
+                    if (res.a % i == 0 && res.b % i == 0)
+
+                    {
+
+                        res.a = res.a / i;
+
+                        res.b = res.b / i;
+
+                    }
+
+                }
+
+            }
+
+
+
+            return res;
+
+        }
+
+
+
+        public static Cplex operator -(Cplex x, Cplex y)
+
+        {
+
+
+
+            Cplex resvych = new Cplex();
+
+
+
+            resvych.a = y.b * x.a - x.b * y.a;
+
+            resvych.b = x.b * y.b;
+
+
+
+            if (resvych.a > 0)
+
+            {
+
+
+
+                if (resvych.a > resvych.b)
+
+                {
+
+                    for (int i = 2; i <= resvych.b; ++i)
+
+                    {
+
+                        if (resvych.a % i == 0 && resvych.b % i == 0)
+
+                        {
+
+                            while (resvych.a % i == 0 && (resvych.b) % i == 0)
+
+                            {
+
+                                resvych.a = resvych.a / i;
+
+                                resvych.b = resvych.b / i;
+
+                            }
+
+                        }
+
+                    }
+
+                }
+
+                else if (resvych.a < resvych.b)
+
+                {
+
+                    for (int i = 2; i <= resvych.a; ++i)
+
+                    {
+
+                        if (resvych.a % i == 0 && resvych.b % i == 0)
+
+                        {
+
+                            while (resvych.a % i == 0 && resvych.b % i == 0)
+
+                            {
+
+                                resvych.a = resvych.a / i;
+
+                                resvych.b = resvych.b / i;
+
+                            }
+
+                        }
+
+                    }
+
+                }
+
+            }
+
+
+
+            else if (resvych.a < 0)
+
+            {
+
+                if (Math.Abs(resvych.a) > Math.Abs(resvych.b))
+
+                {
+
+                    for (int i = 2; i <= Math.Abs(resvych.b); ++i)
+
+                    {
+
+                        if (resvych.a % i == 0 && resvych.b % i == 0)
+
+                        {
+
+                            while (resvych.a % i == 0 && (resvych.b) % i == 0)
+
+                            {
+
+                                resvych.a = resvych.a / i;
+
+                                resvych.b = resvych.b / i;
+
+                            }
+
+                        }
+
+                    }
+
+                }
+
+                else if (Math.Abs(resvych.a) < Math.Abs(resvych.b))
+
+                {
+
+                    for (int i = 2; i <= Math.Abs(resvych.a); ++i)
+
+                    {
+
+                        if (resvych.a % i == 0 && resvych.b % i == 0)
+
+                        {
+
+                            while (resvych.a % i == 0 && resvych.b % i == 0)
+
+                            {
+
+                                resvych.a = resvych.a / i;
+
+                                resvych.b = resvych.b / i;
+
+                            }
+
+                        }
+
+                    }
+
+                }
+
+            }
+
+            return resvych;
+
+
+
+        }
+
+
+
+        public static Cplex operator /(Cplex x, Cplex y)
+
+        {
+
+            Cplex resdel = new Cplex();
+
+
+
+            resdel.a = x.a * y.b;
+
+            resdel.b = x.b * y.a;
+
+
+
+            if (resdel.a > resdel.b)
+
+            {
+
+                for (int i = 2; i <= resdel.b; ++i)
+
+                {
+
+                    if (resdel.a % i == 0 && resdel.b % i == 0)
+
+                    {
+
+                        while (resdel.a % i == 0 && resdel.b % i == 0)
+
+                        {
+
+                            resdel.a = resdel.a / i;
+
+                            resdel.b = resdel.b / i;
+
+                        }
+
+                    }
+
+                }
+
+            }
+
+            else if (resdel.a < resdel.b)
+
+            {
+
+                for (int i = 2; i <= resdel.a; ++i)
+
+                {
+
+                    if (resdel.a % i == 0 && resdel.b % i == 0)
+
+                    {
+
+                        while (resdel.a % i == 0 && resdel.b % i == 0)
+
+                        {
+
+                            resdel.a = resdel.a / i;
+
+                            resdel.b = resdel.b / i;
+
+                        }
+
+                    }
+
+                }
+
+            }
+
+
+
+            return resdel;
+
+        }
+
+
+
+
+
+        public static Cplex operator *(Cplex x, Cplex y)
+
+        {
+
+            Cplex resm = new Cplex();
+
+            resm.a = x.a * y.a;
+
+            resm.b = x.b * y.b;
+
+
+
+            if (resm.a > resm.b)
+
+            {
+
+                for (int i = 2; i <= resm.b; ++i)
+
+                {
+
+                    if (resm.a % i == 0 && resm.b % i == 0)
+
+                    {
+
+                        while (resm.a % i == 0 && resm.b % i == 0)
+
+                        {
+
+                            resm.a = resm.a / i;
+
+                            resm.b = resm.b / i;
+
+                        }
+
+                    }
+
+                }
+
+            }
+
+            else if (resm.a < resm.b)
+
+            {
+
+                for (int i = 2; i <= resm.a; ++i)
+
+                {
+
+                    if (resm.a % i == 0 && resm.b % i == 0)
+
+                    {
+
+                        while (resm.a % i == 0 && resm.b % i == 0)
+
+                        {
+
+                            resm.a = resm.a / i;
+
+                            resm.b = resm.b / i;
+
+                        }
+
+                    }
+
+                }
+
+            }
+
+            return resm;
+
+        }
+
+
+
+        public override string ToString()
+
+        {
+
+            return a + " " + b;
+
+        }
+
+    }
+
+
+
+}
---
+++ Lab4/Complex/Complex/Program.cs
@@ -0,0 +1,197 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Xml.Serialization;
+
+
+
+namespace complex
+
+{
+
+    [Serializable]
+
+    class Program
+
+    {
+
+        static void Main(string[] args)
+
+        {
+
+            F1();
+
+        }
+
+      
+
+
+
+        private static void F1()
+
+        {
+
+            Console.WriteLine("Write the numeratore and denominator a: ");
+
+            string line1 = Console.ReadLine();
+
+            string[] linefor1 = line1.Split(' ');
+
+
+
+            int a = int.Parse(linefor1[0]);
+
+            int b = int.Parse(linefor1[1]);
+
+            Console.WriteLine("Write a sign: ");
+
+            string sign = Console.ReadLine();
+
+            char znak = sign[0];
+
+            Console.WriteLine("Write the numeratore and denominator b: ");
+
+            string line2 = Console.ReadLine();
+
+            string[] linefor2 = line2.Split(' ');
+
+
+
+            int c = int.Parse(linefor2[0]);
+
+            int d = int.Parse(linefor2[1]);
+
+
+
+            Cplex c1 = new Cplex(a, b);
+
+            Cplex c2 = new Cplex(c, d);
+
+            Cplex result = c1 + c2;
+
+            Cplex result1 = c1 - c2;
+
+            Cplex resultdiv = c1 / c2;
+
+            Cplex resultm = c1 * c2;
+
+            if (znak == '+')
+            {
+
+                string summa = "Sum = " + result;
+
+                Console.WriteLine(summa);
+
+                FileStream fs = new FileStream("complex.xml", FileMode.Truncate, FileAccess.ReadWrite);
+
+                XmlSerializer xs = new XmlSerializer(typeof(string));
+
+
+
+                xs.Serialize(fs, summa);
+
+
+
+
+
+                fs.Close();
+
+            }
+
+            if (znak == '-')
+            {
+
+                string raznost = "Diference = " + result1;
+
+                Console.WriteLine(raznost);
+
+                FileStream fs = new FileStream("complex.xml", FileMode.Truncate, FileAccess.ReadWrite);
+
+                XmlSerializer xs = new XmlSerializer(typeof(string));
+
+
+
+                xs.Serialize(fs, raznost);
+
+
+
+
+
+                fs.Close();
+
+            }
+
+            if (znak == '*')
+            {
+
+                string multi = "Multiplication = " + resultm;
+
+                Console.WriteLine(multi);
+
+                FileStream fs = new FileStream("complex.xml", FileMode.Truncate, FileAccess.ReadWrite);
+
+                XmlSerializer xs = new XmlSerializer(typeof(string));
+
+
+
+                xs.Serialize(fs, multi);
+
+
+
+
+
+                fs.Close();
+
+            }
+
+            if (znak == '/')
+            {
+
+                string delenie = "Division = " + resultdiv;
+
+                Console.WriteLine(delenie);
+
+                FileStream fs = new FileStream("complex.xml", FileMode.Truncate, FileAccess.ReadWrite);
+
+                XmlSerializer xs = new XmlSerializer(typeof(string));
+
+
+
+                xs.Serialize(fs, delenie);
+
+
+
+
+
+                fs.Close();
+
+            }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            Console.ReadKey();
+
+
+
+
+
+        }
+
+    }
+
+}
