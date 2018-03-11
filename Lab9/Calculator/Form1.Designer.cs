f1714d553d982f46bf9d922dc3ba19100040e304
Iseke <Islamabdukarimov1999@mail.ru>
2018-03-11 16:28:12 +0600

    asd

---
+++ Lab9/Calculator/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace Calculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Name = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+            this.components.Add(button_DivisiontoOne);
+            this.components.Add(button_DivisiontoOne);
+
+            this.components.Add(button_DivisiontoOne);
+
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button_DivisiontoOne;
+        private System.Windows.Forms.Button button_Root;
+        private System.Windows.Forms.Button button_PowerofTwo;
+        private System.Windows.Forms.Button button_Division;
+        private System.Windows.Forms.Button button_Delete;
+        private System.Windows.Forms.Button button_Clean;
+        private System.Windows.Forms.Button button_CleanEntry;
+        private System.Windows.Forms.Button button_Minus;
+        private System.Windows.Forms.Button button_10;
+        private System.Windows.Forms.Button button_5;
+        private System.Windows.Forms.Button button_4;
+        private System.Windows.Forms.Button button_Multiplication;
+        private System.Windows.Forms.Button button_6;
+        private System.Windows.Forms.Button button_8;
+        private System.Windows.Forms.Button button_7;
+        private System.Windows.Forms.Button button_Result;
+        private System.Windows.Forms.Button button_Dot;
+        private System.Windows.Forms.Button button_Negation;
+        private System.Windows.Forms.Button button_0;
+        private System.Windows.Forms.Button button_Plus;
+        private System.Windows.Forms.Button button_3;
+        private System.Windows.Forms.Button button_2;
+        private System.Windows.Forms.Button button_1;
+        private System.Windows.Forms.Button button_MemoryClean;
+        private System.Windows.Forms.Button button_MemoryRead;
+        private System.Windows.Forms.Button button_MemoryMinus;
+        private System.Windows.Forms.Button button_MemoryPlus;
+        private System.Windows.Forms.Button button_MemorySave;
+        private System.Windows.Forms.Button button_MemoryView;
+        private System.Windows.Forms.Button button31;
+        private System.Windows.Forms.LinkLabel linkLabelSign;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.TextBox ResultTextBox1;
+
+        
+    }
+}
+
---
+++ Lab9/Calculator/Form1.cs
@@ -0,0 +1,199 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculator
+{
+    public partial class Form1 : Form
+    {
+        double ResultValue = 0;
+        string OperationSign = "";
+        bool IsOperationPerfomed = false;
+        bool IspressEqual = false;
+        bool IsSignPressed = false;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        
+
+
+        private void button_Click(object sender, EventArgs e)
+        {
+            IsSignPressed = true;
+            Button button = (Button)sender;
+            if (IspressEqual)
+            {
+                if (IsOperationPerfomed)
+                {
+                    ResultTextBox1.Clear();
+                    ResultTextBox1.Text = "0";
+                    ResultValue = 0;
+                }
+            }
+            IspressEqual = false;
+
+            if (ResultTextBox1.Text == "0")
+            {
+                if(button.Text == ",")
+                {
+                    ResultTextBox1.Text = ResultTextBox1.Text + button.Text;
+                }
+            }
+            
+            if (ResultTextBox1.Text == "0" || IsOperationPerfomed)
+            {
+                ResultTextBox1.Clear();
+            }
+            IsOperationPerfomed = false;
+            
+            if (button.Text == ",")
+            {
+                if (!ResultTextBox1.Text.Contains(","))
+                {
+                    ResultTextBox1.Text += ",";
+                   
+
+                }
+            }
+            else
+            ResultTextBox1.Text = ResultTextBox1.Text + button.Text;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void operated_click(object sender, EventArgs e)
+        {
+            IspressEqual = false;
+            IsSignPressed = false;
+
+            Button button = (Button)sender;
+            if (ResultValue != 0)
+            {
+                button_Result.PerformClick();
+                IsOperationPerfomed = true;
+               // ResultValue = double.Parse(ResultTextBox1.Text);
+                OperationSign = button.Text;
+               
+            }
+            else
+            {
+                IsOperationPerfomed = true;
+                ResultValue = double.Parse(ResultTextBox1.Text);
+                OperationSign = button.Text;
+            }
+            if(  button.Text == "√")
+            {
+                linkLabelSign.Text = OperationSign + "(" + ResultValue+")";
+            }
+            else if(button.Text == "x²")
+            {
+                linkLabelSign.Text = ResultValue + "²";
+            }
+            else if(button.Text == "1/x")
+            {
+                linkLabelSign.Text = "1/" + ResultValue;
+            }
+            else
+            {
+                linkLabelSign.Text = ResultValue + " " + OperationSign;
+
+            }
+        }
+
+        private void result_click(object sender, EventArgs e)
+        {
+            IsSignPressed = false;
+            IsOperationPerfomed = false;
+            IspressEqual = true;
+            switch (OperationSign)
+            {
+                case "+":
+                    ResultTextBox1.Text = (ResultValue + double.Parse(ResultTextBox1.Text)).ToString();
+                    break;
+                case "-":
+                    ResultTextBox1.Text = (ResultValue - double.Parse(ResultTextBox1.Text)).ToString();
+                    break;
+                case "÷":
+                    ResultTextBox1.Text = (ResultValue / double.Parse(ResultTextBox1.Text)).ToString();
+                    break;
+                case "X":
+                    ResultTextBox1.Text = (ResultValue * double.Parse(ResultTextBox1.Text)).ToString();
+                    break;
+                case "x²":
+                    ResultTextBox1.Text = (Math.Pow(ResultValue ,2)).ToString();
+                    break;
+                case "√":
+                    ResultTextBox1.Text = (Math.Sqrt(  ResultValue)).ToString();
+                    break;
+                case "1/x":
+                    ResultTextBox1.Text = (1/ResultValue).ToString();
+                    break;
+                default:
+                    break;
+            }
+            ResultValue = double.Parse(ResultTextBox1.Text);
+            linkLabelSign.Text = "";
+        }
+
+        private void ClickButton_ClearEntry(object sender, EventArgs e)
+        {
+            ResultTextBox1.Text = "0";
+        }
+
+        private void ClickButton_Clean(object sender, EventArgs e)
+        {
+            ResultTextBox1.Text = "0";
+            ResultValue = 0;
+            linkLabelSign.Text = "";
+
+        }
+
+        private void Click_Delete(object sender, EventArgs e)
+        {
+            string k = "";
+            string s = ResultTextBox1.Text;
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (i == s.Length - 1) { continue; }
+                
+                    k += s[i];
+                
+            }
+            ResultTextBox1.Text = k;
+        }
+
+        private void button_Negation_Click(object sender, EventArgs e)
+        {
+            if (double.Parse(ResultTextBox1.Text) > 0)
+            {
+                ResultTextBox1.Text = (-(double.Parse(ResultTextBox1.Text))).ToString();
+            }
+            else if (double.Parse(ResultTextBox1.Text) < 0)
+            {
+                ResultTextBox1.Text = (Math.Abs(double.Parse(ResultTextBox1.Text))).ToString();
+
+            }
+            else
+            {
+                ResultTextBox1.Text = "-";
+            }
+        }
+        
+
+
+       
+    }
+}
